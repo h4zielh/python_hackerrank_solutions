97d734e8ddf0eb850f26622637fbc6b52a98c038
h4ziel <hazielhayan6@gmail.com>
2024-05-18 22:25:31 -0300

    aaa

--- solutions/get_highest_score.py
+++ solutions/get_highest_score.py
@@ -1,3 +1,12 @@
 """
 return the highest score from list, pretty simple
 """
+
+def get_highest_score(num: int, scores: list) -> int:
+    """return the highest score from list."""
+
+    pass
+
+if __name__ == "name":
+    n = int(input())
+    arr = map(int, input().split())
